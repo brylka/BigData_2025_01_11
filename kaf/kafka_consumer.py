a588fd498c6136ee0f488b945543a2e1e866fe88
Bartosz Bryniarski <brylka@jelenia.com>
2025-01-12 14:01:57 +0100

    niedziela

--- kaf/kafka_consumer.py
+++ kaf/kafka_consumer.py
@@ -1,4 +1,4 @@
-from confluent_kafka import Consumer
+from confluent_kafka import Consumer, KafkaError
 import json
 
 
@@ -11,3 +11,26 @@ class WeatherDataConsumer:
         }
         self.consumer = Consumer(conf)
         self.consumer.subscribe(['weather_data'])
+        print(f"Konsument zaincjowany...")
+
+    def start_consuming(self):
+        while True:
+            msg = self.consumer.poll(1.0)
+
+            if msg is None:
+                continue
+            if msg.error():
+                if msg.error().code() == KafkaError._PARTITION_EOF:
+                    continue
+                else:
+                    print(f"Błąd: {msg.error()}")
+                    break
+
+            data = json.loads(msg.value().decode("utf-8"))
+            station_id = data['station_id']
+            print(f"Otrzymano zadanie dla stacji: {station_id}")
+
+
+if __name__ == "__main__":
+    consumer = WeatherDataConsumer()
+    consumer.start_consuming()
