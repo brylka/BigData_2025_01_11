ae07fd06359b9d2d29dcabb30edb9c10f2296cdb
Bartosz Bryniarski <brylka@jelenia.com>
2025-01-12 09:56:54 +0100

    niedziela

--- red/tasks.py
+++ red/tasks.py
@@ -2,17 +2,28 @@ import requests
 
 def fetch_weather_data(station_id):
 
-    print(f"Pobieranie danych dla stacji {station_id}...")
+    try:
+        print(f"Pobieranie danych dla stacji {station_id}...")
 
-    response = requests.get(f"http://127.0.0.1:8000/weather/{station_id}")
+        response = requests.get(f"http://127.0.0.1:8000/weather/{station_id}")
 
-    if response.status_code == 200:
-        data = response.json()
-        temp = float(data["temperature"])
-        print(temp)
+        if response.status_code == 200:
+            data = response.json()
+            temp = float(data["temperature"])
 
-    else:
-        pass
+            if temp > 30:
+                print(f"Wysoka temperatura {temp}'C na stacji {station_id}")
+            return data
+
+        else:
+            print(f"Błąd API, kod odpowiedzi: {response.status_code}")
+            return None
+    except requests.exceptions.ConnectionError:
+        print(f"Nie można połączyć się z API dla stacji {station_id}")
+        return None
+    except Exception as e:
+        print(f"Błąd dla stacji {station_id} {e}")
+        return None
 
 if __name__ == "__main__":
-    fetch_weather_data("STACJA_TESTOWA")
+    print(fetch_weather_data("STACJA_TESTOWA"))
