d9f9e40ba71e7de95c5a5ca60da6bb5884285ae0
Bartosz Bryniarski <brylka@jelenia.com>
2025-01-12 14:39:43 +0100

    niedziela

--- kaf/kafka_consumer.py
+++ kaf/kafka_consumer.py
@@ -15,27 +15,34 @@ class WeatherDataConsumer:
         print(f"Konsument zaincjowany...")
 
     def start_consuming(self):
-        while True:
-            msg = self.consumer.poll(1.0)
+        with open('weather_data.csv', 'a') as f:
+            if f.tell() == 0:
+                f.write("timestamp,station_id,temperature\n")
+            while True:
+                msg = self.consumer.poll(1.0)
 
-            if msg is None:
-                continue
-            if msg.error():
-                if msg.error().code() == KafkaError._PARTITION_EOF:
+                if msg is None:
                     continue
-                else:
-                    print(f"Błąd: {msg.error()}")
-                    break
-            try:
-                data = json.loads(msg.value().decode("utf-8"))
-                station_id = data['station_id']
-                print(f"Otrzymano zadanie dla stacji: {station_id}")
+                if msg.error():
+                    if msg.error().code() == KafkaError._PARTITION_EOF:
+                        continue
+                    else:
+                        print(f"Błąd: {msg.error()}")
+                        break
+                try:
+                    data = json.loads(msg.value().decode("utf-8"))
+                    station_id = data['station_id']
+                    print(f"Otrzymano zadanie dla stacji: {station_id}")
 
-                weather_data = fetch_weather_data(station_id, False)
-                if weather_data:
-                    print(f"Pobrano dane: {weather_data}")
-            except Exception as e:
-                print(f"Błąd przetwarzania wiadomości: {e}")
+                    weather_data = fetch_weather_data(station_id, False)
+                    if weather_data:
+                        timestamp = weather_data['timestamp']
+                        temp = weather_data['temperature']
+                        f.write(f"{timestamp},{station_id},{temp}\n")
+                        f.flush()
+                        print(f"Pobrano dane: {weather_data}")
+                except Exception as e:
+                    print(f"Błąd przetwarzania wiadomości: {e}")
 
 
 if __name__ == "__main__":
